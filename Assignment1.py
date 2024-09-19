d3cd8fa1ecfee284dcb12882197d06dd9cd91d2a
Karan Sharma <89495471+karansharmaa@users.noreply.github.com>
2024-09-18 18:50:49 -0700

    Update Assignment1.py
    Some formatting, working on fixing merge sort - it appears it might be broken.

--- Assignment1.py
+++ Assignment1.py
@@ -36,34 +36,53 @@ def merge_sort(arr):
             j += 1
             k += 1
 
-# Array sizes to test
-sizes = np.array([100, 200, 400, 800, 1600, 3200, 5000])
+ 
+#using the matplotlib library, we will plot the runtimes of the algorithms and show the 
+#difference between time complexities.            
+def plotOnGraph(sizes, bubble_times, merge_times): 
+    # Plotting the results
+    plt.figure(figsize=(10,6))
+    plt.plot(sizes, bubble_times, label='Bubble Sort (O(n^2))', marker='o')
+    plt.plot(sizes, merge_times, label='Merge Sort (O(n log n))', marker='o')
+    plt.title('Sorting Algorithms Time Complexity')
+    plt.xlabel('Input Size (n)')
+    plt.ylabel('Time (seconds)')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+def main(): 
+    # We will be using arrays of different sizes as below and to test out sorting speed. 
+    sizes = np.array([100, 200, 400, 800, 1600, 3200, 5000])
 
-# To store runtimes
-bubble_times = []
-merge_times = []
+    # These will be used to store the time used by sorting of each iteration of the array size per algorithm
+    bubble_times = []
+    merge_times = []
 
-# Measure time for Bubble Sort
-for size in sizes:
-    arr = np.random.randint(0, 10000, size)
-    start_time = time.time()
-    bubble_sort(arr.copy())
-    bubble_times.append(time.time() - start_time)
+    # Measure time for Bubble Sort - these times will be stored in bubble_times 
+    #and be plotted  into the graph we will use to show runtimes. 
+    for size in sizes:
+        #random integers between 0 and 10000 will be choosen and put in to whatever size array is being iterated
+        #into the for loop. 
+        arr = np.random.randint(0, 10000, size)
+        #using time library, we will calculate how long it takes to sort using bubble sort. 
+        start_time = time.time()
+        bubble_sort(arr.copy())
+        bubble_times.append(time.time() - start_time)
 
-# Measure time for Merge Sort
-for size in sizes:
-    arr = np.random.randint(0, 10000, size)
-    start_time = time.time()
-    merge_sort(arr.copy())
-    merge_times.append(time.time() - start_time)
+    # Measure time for Bubble Sort - these times will be stored in bubble_times 
+    #and be plotted  into the graph we will use to show runtimes. 
+    for size in sizes:
+        #random integers between 0 and 10000 will be choosen and put in to whatever size array is being iterated
+        #into the for loop. 
+        arr = np.random.randint(0, 10000, size)
+        #using time library, we will calculate how long it takes to sort using merge sort
+        start_time = time.time()
+        merge_sort(arr.copy())
+        merge_times.append(time.time() - start_time)
+        
+    plotOnGraph(sizes, bubble_times, merge_times)
 
-# Plotting the results
-plt.figure(figsize=(10,6))
-plt.plot(sizes, bubble_times, label='Bubble Sort (O(n^2))', marker='o')
-plt.plot(sizes, merge_times, label='Merge Sort (O(n log n))', marker='o')
-plt.title('Sorting Algorithms Time Complexity')
-plt.xlabel('Input Size (n)')
-plt.ylabel('Time (seconds)')
-plt.legend()
-plt.grid(True)
-plt.show()
+
+
+main()
